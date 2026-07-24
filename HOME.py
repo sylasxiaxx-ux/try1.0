97d217db1d1a2ecb07f4e8d0b0c8963600ea9f60
sylasxiaxx-ux <sylasxiaxx@gmail.com>
2026-07-24 14:56:41 +0800

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_shortcuts import clear_shortcuts
 clear_shortcuts()
+
 st.set_page_config(page_title="分析工具集", layout="wide")
 st.title("🧰 分析工具菜单")
 st.markdown("请从左侧侧边栏选择需要使用的工具。")
